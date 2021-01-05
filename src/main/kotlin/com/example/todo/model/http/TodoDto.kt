b5fc7ab45d14e4b607421dd8376038c66d59ceb7
2rohyun <dlehgus5656@naver.com>
2021-01-05 16:29:54 +0900

    ADD swagger configuration

--- src/main/kotlin/com/example/todo/model/http/TodoDto.kt
+++ src/main/kotlin/com/example/todo/model/http/TodoDto.kt
@@ -1,6 +1,7 @@
 package com.example.todo.model.http
 
 import com.example.todo.database.Todo
+import io.swagger.annotations.ApiModelProperty
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import javax.validation.constraints.AssertTrue
@@ -8,19 +9,47 @@ import javax.validation.constraints.NotBlank
 
 data class TodoDto(
 
+    @field:ApiModelProperty(
+        value = "DB INDEX",
+        example = "1",
+        required = false
+    )
     var index: Int?=null,
 
+    @field:ApiModelProperty(
+        value = "일정명",
+        example = "일정관리",
+        required = true
+    )
     @field:NotBlank
     var title: String?=null,
 
+    @field:ApiModelProperty(
+        value = "일정설명",
+        example = "13시 스타벅스",
+        required = false
+    )
     var description: String?=null,
 
+    @field:ApiModelProperty(
+        value = "시간",
+        example = "2021-01-05 13:00:00",
+        required = true
+    )
     @field:NotBlank
     //yyyy-MM-dd HH:mm:ss
     var schedule: String?=null,
 
+    @field:ApiModelProperty(
+        value = "생성일자",
+        required = false
+    )
     var createdAt: LocalDateTime?=null,
 
+    @field:ApiModelProperty(
+        value = "변경일자",
+        required = false
+    )
     var updatedAt: LocalDateTime?=null
 
 ) {
